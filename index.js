5147b826ea514ba60f2dcdb02d8918dcd800a641
NostalgicSU <101046908+NostalgicSU@users.noreply.github.com>
2024-07-04 22:18:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let btn = document.getElementsByClassName("guzicek")
+
+btn[0].addEventListener("click", function(){window.open("https://www.youtube.com/watch?v=uHgt8giw1LY&ab_channel=Licale")})
+
+
